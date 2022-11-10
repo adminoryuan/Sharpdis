97f4c7d47d0a5a6dd83b5affdcc0d132621b20b2
adminoryuan <1317332365@qq.com>
2022-11-10 13:13:36 +0800

    update test

---
+++ Sharpdis.Test/DataStructure/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sharpdis.Test.DataStructure
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
