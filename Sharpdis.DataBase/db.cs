2a9b69d4f1c85d0b6fa765716d57ac0bc53d89c9
adminoryuan <1317332365@qq.com>
2022-11-29 13:47:52 +0800

    Add expire

--- Sharpdis.DataBase/db.cs
+++ Sharpdis.DataBase/db.cs
@@ -1,6 +1,9 @@
 ﻿using Sharpdis.Common;
 using Sharpdis.Common.Entity;
+using Sharpdis.DataBase.expire;
+using Sharpdis.DataBase.expire.impl;
 using Sharpdis.DataStructure.structure;
+using Sharpdis.Untils;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,7 +13,8 @@ namespace Sharpdis.DataStructure
    public  class  Database
     {
 
-        private static Database instan = null;
+        private static Database instan;
+        private static IExpireFactory factory;
 
         public static  Database newInstance()
         {
@@ -36,14 +40,12 @@ namespace Sharpdis.DataStructure
         {
             selectIndex = Global.config.DataBase;
             dbs = new Dictionary<string, Structure?>[MAXdb];
+
+            factory = IExpireFactory.getExpire(Global.config.ExpireType);
             for (int i = 0; i < MAXdb; i++) 
                 dbs[i] = new Dictionary<string, Structure?>();
         }
-        private  Database()
-        {
-
-           
-        }
+        private Database() { }
 
         /// <summary>
         /// 如何不存在则创建
@@ -53,8 +55,11 @@ namespace Sharpdis.DataStructure
         /// <returns></returns>
         public T? CrateStrucutr<T>(string key) where T : Structure
         {
-            var val = dbs[selectIndex].GetValueOrDefault(key,null);
 
+            Expire(key);
+
+            var val = dbs[selectIndex].GetValueOrDefault(key,null);
+            
             if (val == null)
             {
                 //判断数据结构类型
@@ -74,10 +79,21 @@ namespace Sharpdis.DataStructure
             
             return val==null?null:(T)val;
         }
+        private void Expire(string key)
+        {
+            //判断是否包括惰性加载
+            if (factory is LazyFactory)
+                factory.CheckExpire(key);
+        }
 
         public int GetSelectIndex()
         {
             return selectIndex;
+        }
+
+        public void Del(string key)
+        {
+             dbs[selectIndex].Remove(key);
         }
 
         /// <summary>
@@ -88,6 +104,40 @@ namespace Sharpdis.DataStructure
 
         public  Structure? getStrucutr(string key, int selectIndex)
         {
+            Expire(key);
+
+            return dbs[selectIndex]?.GetValueOrDefault(key, null);
+        
+        }
+
+        /// <summary>
+        /// 检测是否过期
+        /// </summary>
+        /// <param name="key"></param>
+        public void CheckExpire(string key)
+        {
+            var l = dbs[selectIndex]?.GetValueOrDefault(key);
+
+            if (l != null && l.expire!=-1)
+            {
+                var currTimeSpan = TimeUntils.getTimeSpan();
+                if (l.expire + l.start < currTimeSpan)
+                {
+                    //过期了需要删除
+                    CmdTable.db.Del(key);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 不存在则返回null
+        /// </summary>
+        /// <param name="key">建名</param>
+        /// <returns></returns>
+        public Structure? getStrucutr(string key)
+        {
+            Expire(key);
+            factory.CheckExpire(key);
             return dbs[selectIndex]?.GetValueOrDefault(key, null);
         }
     }
---
+++ Sharpdis.Untils/TimeUntils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharpdis.Untils
+{
+    public static class TimeUntils
+    {
+
+        public static long getTimeSpan()
+        {
+            TimeSpan mTimeSpan = DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0);
+            //得到精确到秒的时间戳（长度10位）
+            long time = (long)mTimeSpan.TotalSeconds;
+            return time;
+        }
+    }
+}
