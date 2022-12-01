e31819d629f2e49ad7ca9c5469e110c1b0798062
adminoryuan <1317332365@qq.com>
2022-12-01 21:59:11 +0800

    add log

---
+++ Sharpdis.Log/absLogger.cs
@@ -0,0 +1,47 @@
+﻿using Sharpdis.Common.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpdis.Log
+{
+    public abstract class absLogger
+    {
+        protected string _fileName;
+       
+        public absLogger(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        protected async Task WriteAsync(byte[] body)
+        {
+                var _file = File.Open(_fileName, FileMode.OpenOrCreate | FileMode.Append); 
+                using (var stream = new StreamWriter(_file))
+                {
+                    stream.WriteLine(Encoding.UTF8.GetString(body));
+                    await stream.FlushAsync();
+                }
+                _file.Close();
+
+        }
+        /// <summary>
+        /// 写入日志
+        /// </summary>
+        /// <param name="cmd"></param>
+        public abstract void AppendLogAsync(RespRequestEntity req);
+
+        /// <summary>
+        /// 加载日志
+        /// </summary>
+        public abstract void LoadLog(Action<Common.Entity.RespRequestEntity> func);
+
+
+
+
+    }
+}
+
+
---
+++ Sharpdis.Log/impl/AofLogger.cs
@@ -0,0 +1,62 @@
+﻿using DotNetty.Buffers;
+using Sharpdis.Common.Entity;
+using Sharpdis.Log.parser;
+using Sharpdis.Untils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
+
+namespace Sharpdis.Log.impl
+{
+    public class AofLogger : absLogger
+    {
+        private static AofLogger? instance=null;
+
+        private AofLogger() : base("appendonly.aof")
+        {
+
+        }
+
+        public static absLogger getAofInstance()
+        {
+            if (instance == null)
+                instance = new AofLogger();
+            return instance;
+        }
+
+        public override async void AppendLogAsync(RespRequestEntity req)
+        {
+             if(LoggerUntils.IsWriteCmd(req.headers))
+                await WriteAsync(req.respBody);
+        }
+
+        /// <summary>
+        /// 加载aof 日志
+        /// </summary>
+        /// <param name="func">解析出命令后回调</param>
+        public override void LoadLog(Action<Common.Entity.RespRequestEntity> func)
+        {
+            try { 
+            if (!File.Exists(_fileName))
+                return;
+                var body= File.ReadAllBytes(_fileName);
+
+                var buf = Unpooled.Buffer();
+            
+                buf.WriteBytes(body);
+            
+                //解析日志
+                RespParser.Parser(buf, func);
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+ 
+    }
+}
